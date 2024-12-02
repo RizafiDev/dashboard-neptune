1bcd838b1ee57fb55f27c56bce6b99364c678c2d
RizafiDev <firmansyah.riza6@smk.belajar.id>
2024-12-02 22:37:12 +0700

    turuu

--- config/app.php
+++ config/app.php
@@ -13,7 +13,8 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'OrionsMelody'),
+    'logo' => env('APP_LOGO', 'OrionsMelody'),
 
     /*
     |--------------------------------------------------------------------------
